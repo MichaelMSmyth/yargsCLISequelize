2dc22cf9787fc44911b6999cd7fb770f386636a5
Michael Smyth <michael.m.smyth@gmail.com>
2021-12-24 11:12:24 +0000

    fixed packages uploaded to git

--- src/app.js
+++ src/app.js
@@ -9,3 +9,69 @@ const { DatabaseError } = require("sequelize/dist")
 
 // Stretch goal - Add a User table that is connected to movie entries to indicate who added the movie.
 // To be done by Tuesday 4th Jan
+
+const app = async (args) => {
+
+    // Take the args object and slice it into seperate objects representing the command and their input data
+    // Data to be input to the db should be one object: inputObj{}
+    // Data to filter by should be one object: filterObj{}
+    // Data to sort by should be one object: sortObj
+  
+    // Dot notation can be used with yargs so I can seperate input data from filter and sort in the cli
+    //e.g. node src/app.js --create.title="Movie title" --create.actor="Johhny Megastar"
+  
+    try {
+  
+      console.log(args)
+  
+      if (args.create) {
+  
+        // The full command is: node src/app.js --create.title="Movie Title" --create.actor="Johhny Megastar"
+        // This will create a record with the input fields { title: 'Movie Title', actor: 'Johhny Megastar' }
+  
+        await createRecord(args.create);
+  
+      } else if (args.read) {
+  
+        // The command to read is: node src/app.js --read.title="Movie Title" --read.actor="Johhny Megastar"
+        // This will return all records matching the fields { title: 'Movie Title', actor: 'Johhny Megastar' }
+  
+        // The command to read a single record by it's id is: node src/app.js --read._id="<id number>"
+  
+        // --sort is an optional argument which will sort the returned result. "1" is in ascending order
+        // The full command to read and sort is: node src/app.js --read.actor="Johhny Megastar" --sort.rating="ascending"
+  
+        // If there are no fields in args.read then it will read all records
+        // e.g. --read
+        // or: --read --sort.title="ascending"
+  
+        await readRecord(args.read, args.sort)
+  
+      } else if (args.update) {
+  
+        // The command to update is: node src/app.js --update.actor="Replacement Dude" --filter.title="Movie Title"
+        // This will update all records matching the filter with the data in the update object
+        // If you want to update a single record then target that record by id or be very specific with search terms.
+        // Record id is returned by --read
+  
+        await updateRecord(args.update, args.filter);
+  
+      } else if (args.delete) {
+  
+        // The command to delete is: node src/app.js --delete.id="61bfba994522c89d9654e95f"
+        // This will delete the record matching the id
+        // Record id is returned by --read
+  
+        await deleteRecord(args.delete);
+  
+      } else {
+        console.log("Incorrect command");
+        mongoose.disconnect();
+      }
+    } catch (error) {
+      console.log(error);
+      mongoose.disconnect();
+    }
+  };
+  
+  app(yargs.argv);
